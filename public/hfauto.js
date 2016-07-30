91870bddaa611b5eea145217bbb1a4438d0c1697
Russell Dwarshuis <kb8u_vhf@hotmail.com>
2016-07-30 15:12:03 -0400

    dials resize when viewport is changed

--- public/hfauto.js
+++ public/hfauto.js
@@ -1,3 +1,4 @@
+(function($, viewport){ $(document).ready(function() {
 google.charts.load('current', {'packages':['gauge']});
 google.charts.setOnLoadCallback(drawChart);
 
@@ -6,71 +7,84 @@ var highPowerTicks = ['0','300','600','900','1200','1500','1800'];
 var highYellowFrom = 1500;
 var lowMaxPower = 120;
 var lowPowerTicks = ['0','20','40','60','80','100','120'];
-var dialWidth = 400;
-var dialHeight = 120;
+var defaultWidth = 200;
+var defaultHeight = 200;
+
+var SWRoptions = {
+  width: defaultWidth, height: defaultHeight,
+  min: 1, max: 3,
+  minorTicks: 5,
+  majorTicks: ['1','1.5','2','2.5','3'],
+  greenFrom: 1, greenTo: 1.5,
+  yellowFrom: 1.5, yellowTo: 2,
+  redFrom: 2, redTo: 6
+};
+
+var Loptions = {
+  width: defaultWidth, height: defaultHeight,
+  min: 137, max: 576,
+  minorTicks: 5
+};
+
+var Coptions = {
+  width: defaultWidth, height: defaultHeight,
+  min: 54, max: 203,
+  minorTicks: 5
+};
+
+var Poweroptions = {
+  width: defaultWidth, height: defaultHeight,
+  min: 0, max: highMaxPower,
+  minorTicks: 3,
+  majorTicks: highPowerTicks,
+  yellowFrom: highYellowFrom, yellowTo: highMaxPower
+};
+
+var PeakPoweroptions = {
+  width: defaultWidth, height: defaultHeight,
+  min: 0, max: highMaxPower,
+  minorTicks: 3,
+  majorTicks: highPowerTicks,
+  yellowFrom: highYellowFrom, yellowTo: highMaxPower
+};
+
+function resizeDials(size) {
+  SWRoptions['width'] = SWRoptions['height'] = size;
+  Loptions['width'] = Loptions['height'] = size;
+  Coptions['width'] = Coptions['height'] = size;
+  Poweroptions['width'] = Poweroptions['height'] = size;
+  PeakPoweroptions['width'] = PeakPoweroptions['height'] = size;
+}
 
 function drawChart() {
   var SWRdata = google.visualization.arrayToDataTable([
     ['Label', 'Value'],
     ['SWR', 10],
   ]);
-  var SWRoptions = {
-    width: dialWidth, height: dialHeight,
-    min: 1, max: 3,
-    minorTicks: 5,
-    majorTicks: ['1','1.5','2','2.5','3'],
-    greenFrom: 1, greenTo: 1.5,
-    yellowFrom: 1.5, yellowTo: 2,
-    redFrom: 2, redTo: 6
-  };
   var SWRchart = new google.visualization.Gauge(document.getElementById('SWR'));
 
   var Ldata = google.visualization.arrayToDataTable([
     ['Label', 'Value'],
     ['Inductance', 137]
   ]);
-  var Loptions = {
-    width: dialWidth, height: dialHeight,
-    min: 137, max: 576,
-    minorTicks: 5
-  };
   var Lchart = new google.visualization.Gauge(document.getElementById('L'));
 
   var Cdata = google.visualization.arrayToDataTable([
     ['Label', 'Value'],
     ['Capacitance', 54],
   ]);
-  var Coptions = {
-    width: dialWidth, height: dialHeight,
-    min: 54, max: 203,
-    minorTicks: 5
-  };
   var Cchart = new google.visualization.Gauge(document.getElementById('C'));
 
   var Powerdata = google.visualization.arrayToDataTable([
     ['Label', 'Value'],
     ['Power', 0],
   ]);
-  var Poweroptions = {
-    width: dialWidth, height: dialHeight,
-    min: 0, max: highMaxPower,
-    minorTicks: 3,
-    majorTicks: highPowerTicks,
-    yellowFrom: highYellowFrom, yellowTo: highMaxPower
-  };
   var Powerchart = new google.visualization.Gauge(document.getElementById('Power'));
 
   var PeakPowerdata = google.visualization.arrayToDataTable([
     ['Label', 'Value'],
     ['Power', 0],
   ]);
-  var PeakPoweroptions = {
-    width: dialWidth, height: dialHeight,
-    min: 0, max: highMaxPower,
-    minorTicks: 3,
-    majorTicks: highPowerTicks,
-    yellowFrom: highYellowFrom, yellowTo: highMaxPower
-  };
   var PeakPowerchart = new google.visualization.Gauge(document.getElementById('PeakPower'));
 
   var websocket = new WebSocket(wsUri);
@@ -79,6 +93,9 @@ function drawChart() {
   setInterval(function() { websocket.send('ping'); }, 9000);
 
   function updateDials(j) {
+    if(viewport.is('lg')) { resizeDials(200); }
+    if(viewport.is('md')) { resizeDials(165); }
+    if(viewport.is('<md')) { resizeDials(120); }
     SWRdata.setValue(0, 1, j['ATU_SWR']);
     SWRchart.draw(SWRdata,SWRoptions);
     Ldata.setValue(0, 1, j['ATU_IND']);
@@ -99,6 +116,23 @@ function drawChart() {
     $("#TunerMode").html(j['ATU_OPER_MODE']);
   }
 
+  // Execute code each time window size changes
+  $(window).resize(
+    viewport.changed(function() {
+      function changeto(size) {
+        resizeDials(size);
+        SWRchart.draw(SWRdata,SWRoptions);
+        Lchart.draw(Ldata,Loptions);
+        Cchart.draw(Cdata,Coptions);
+        Powerchart.draw(Powerdata,Poweroptions);
+        PeakPowerchart.draw(PeakPowerdata,PeakPoweroptions);
+      }
+      if(viewport.is('lg')) { changeto(200) }
+      if(viewport.is('md')) { changeto(165) }
+      if(viewport.is('<md')) { changeto(120) }
+    })
+  );
+
   websocket.onmessage = function(evt) {
     if (evt.data === 'keep-alive') { return; }
     var j = JSON.parse(evt.data);
@@ -110,4 +144,4 @@ function drawChart() {
     updateDials(evt);
   };
 }
-
+}); })(jQuery, ResponsiveBootstrapToolkit);
